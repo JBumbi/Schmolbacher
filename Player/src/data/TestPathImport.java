f7dca6d8a1d14922ef030eca630615a94b18da73
vmadmin <vmadmin@192.168.152.133>
2017-06-20 15:36:51 +0200

    push 2

---
+++ Player/src/data/TestPathImport.java
@@ -0,0 +1,35 @@
+package data;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class TestPathImport {
+
+	
+	  public static void main(String[] args) { 
+	        String verzName = "C:\\temp\\test"; 
+	        ArrayList<File> files = getPaths(new File(verzName), 
+	                new ArrayList<File>()); 
+	        if(files == null) return; 
+	        try { 
+	            for (int i = 0; i < files.size(); i++) 
+	                System.out.println(files.get(i).getCanonicalPath()); 
+	        } catch (IOException e) { 
+	            e.printStackTrace(); 
+	        } 
+	    } 
+
+	    private static ArrayList<File> getPaths(File file, ArrayList<File> list) { 
+	        if (file == null || list == null || !file.isDirectory()) 
+	            return null; 
+	        File[] fileArr = file.listFiles(); 
+	        for (File f : fileArr) { 
+	            if (f.isDirectory()) { 
+	                getPaths(f, list); 
+	            } 
+	            list.add(f); 
+	        } 
+	        return list; 
+	    } 
+}
